7a098895ef25294cc8d2f939606137b50eaad0fb
Bruno Cordoba <brunonahuel87@gmail.com>
2024-02-05 15:53:39 -0300

    agregue componente saludo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
+import Saludo from "./components/Saludo";
 
 function App() {
- 
   return (
     <>
-  
-     
+      <Saludo />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Saludo.jsx
@@ -0,0 +1,10 @@
+
+const Saludo = () => {
+    return (
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    );
+};
+
+export default Saludo;
